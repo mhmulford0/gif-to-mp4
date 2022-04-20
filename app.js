11bdb79d87e13b2feec3d325a3274170c68ed9db
Matthew Mulford <mhmulford0@gmail.com>
2022-04-20 15:08:21 -0500

    tweaks

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 const ffmpegInstaller = require("@ffmpeg-installer/ffmpeg");
 const ffprobe = require("@ffprobe-installer/ffprobe");
-const spawn = require("child_process").spawn;
-
-let gifs = ["./big.gif", "./big.gif", "./big.gif", "./big.gif", "./big.gif"];
 
 const ffmpeg = require("fluent-ffmpeg")()
   .setFfprobePath(ffprobe.path)
@@ -32,24 +29,4 @@ function processImage(gifPath, idx) {
   });
 }
 
-// function processGif(gif, quality) {
-//   // const ffmpeg = spawn('ffmpeg', ['-i', `${parent}/${video}.mp4`, '-codec:v', 'libx264', '-profile:v', 'main', '-preset', 'slow', '-b:v', '400k', '-maxrate', '400k', '-bufsize', '800k', '-vf', `scale=-2:${quality}`, '-threads', '0', '-b:a', '128k', `${parent}/transcoded/${video}_${quality}.mp4`]);
-
-//   const p = new Promise((resolve, reject) => {
-//     const ffmpeg = spawn("ffmpeg", [
-//       "-i",
-//       `${gif}`,
-//       "-c:v libx264",
-//       // "-movflags +faststart",
-//       // "-filter:v crop='floor(in_w/2)*2:floor(in_h/2)*2'",
-//       "test.mp4"
-//     ]);
-//     ffmpeg.on("error", (e) => console.log(e));
-//     ffmpeg.on("close", (code) => {
-//       console.log("done")
-//       resolve();
-//     });
-//   });
-//   return p;
-// }
 processImage("mid.gif", 1)
