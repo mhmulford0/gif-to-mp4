a21daaf8b28c11c3daa1f8cbd70a8e34354ecce2
Matthew Mulford <mhmulford0@gmail.com>
2022-04-21 15:22:43 -0500

    queue

--- app.js
+++ app.js
@@ -1,21 +1,17 @@
+const { v4: uuidv4 } = require("uuid");
+
 const ffmpegInstaller = require("@ffmpeg-installer/ffmpeg");
 const ffprobe = require("@ffprobe-installer/ffprobe");
-
-const ffmpeg = require("fluent-ffmpeg")()
-  .setFfprobePath(ffprobe.path)
-  .setFfmpegPath(ffmpegInstaller.path);
 
 const Queue = require("bee-queue");
 const queue = new Queue("convert-gif");
 
 const d = new Date();
 
-function processImage(gifPath, idx) {
-  const job = queue.createJob({ gifPath: "./mid.gif", id: d.getSeconds() });
-  job.save();
-  job.on("succeeded", (result) => {
-    console.log(`Completed Job:  ${job.id}`);
-  });
+function processImage(gifPath) {
+  const ffmpeg = require("fluent-ffmpeg")()
+    .setFfprobePath(ffprobe.path)
+    .setFfmpegPath(ffmpegInstaller.path);
   return new Promise((resolve, reject) => {
     ffmpeg
       .input(gifPath)
@@ -26,7 +22,7 @@ function processImage(gifPath, idx) {
         "-filter:v crop='floor(in_w/2)*2:floor(in_h/2)*2'",
       ])
       .noAudio()
-      .output(`vidgif${idx}.mp4`)
+      .output(`vidgif${uuidv4()}.mp4`)
       .on("end", () => {
         resolve();
       })
@@ -38,11 +34,17 @@ function processImage(gifPath, idx) {
 }
 
 async function main() {
-  await processImage("./mid.gif", d.getSeconds());
-  await processImage("./mid.gif", d.getSeconds() + 10);
+  [...Array(10)].map(() => {
+    const job = queue.createJob({ id: uuidv4() });
+    job.save();
+    job.on("succeeded", (result) => {
+      console.log(`Completed Job:  ${job.id}`);
+    });
+  });
 
-  queue.process(2, async (job, done) => {
+  queue.process(4, async (job, done) => {
     console.log(`Processing job ${job.id}`);
+    await processImage("./mid.gif");
     try {
     } catch (error) {
       console.log(error);
