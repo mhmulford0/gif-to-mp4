00b37a1ddcaf1e0c1f709278395608bb10defea6
Matthew Mulford <mhmulford0@gmail.com>
2022-08-14 13:40:45 -0500

    thread me

--- bull-queue.js
+++ bull-queue.js
@@ -15,29 +15,7 @@ const videoQueue = new Queue(
   "redis://default:QUuFnSn9AwyU88tjfyF9@containers-us-west-73.railway.app:5878"
 );
 
-videoQueue.process((job) => {
-  console.log(job);
-
-  return new Promise((resolve, reject) => {
-    ffmpeg
-      .input("./mid.gif")
-      .outputOptions([
-        "-pix_fmt yuv420p",
-        "-c:v libx264",
-        "-movflags +faststart",
-        "-filter:v crop='floor(in_w/2)*2:floor(in_h/2)*2'",
-      ])
-      .noAudio()
-      .output(`${uuidv4()}.mp4`)
-      .on("end", () => {
-        resolve();
-      })
-      .on("error", (e) => {
-        return reject(new Error(e));
-      })
-      .run();
-  });
-});
+videoQueue.process(2, "./gifProcessor.js");
 
 async function main() {
   const numberOfRuns = [...Array(10)];
