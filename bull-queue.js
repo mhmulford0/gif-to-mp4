5cab1c19bf01fae6b1199b36020037d2d395eb6d
Matthew Mulford <mhmulford0@gmail.com>
2022-08-14 13:37:20 -0500

    promises

--- bull-queue.js
+++ bull-queue.js
@@ -8,40 +8,41 @@ const ffmpeg = require("fluent-ffmpeg")()
   .setFfmpegPath(ffmpegInstaller.path);
 
 // redis://default:QUuFnSn9AwyU88tjfyF9@containers-us-west-73.railway.app:5878
-// 
+//
 
 const videoQueue = new Queue(
   "video transcoding",
   "redis://default:QUuFnSn9AwyU88tjfyF9@containers-us-west-73.railway.app:5878"
 );
 
-videoQueue.process((job, done) => {
+videoQueue.process((job) => {
   console.log(job);
 
-  ffmpeg
-    .input("./big.gif")
-    .outputOptions([
-      "-pix_fmt yuv420p",
-      "-c:v libx264",
-      "-movflags +faststart",
-      "-filter:v crop='floor(in_w/2)*2:floor(in_h/2)*2'",
-    ])
-    .noAudio()
-    .output(`${uuidv4()}.mp4`)
-    .on("end", () => {
-      resolve();
-    })
-    .on("error", (e) => {
-      return reject(new Error(e));
-    })
-    .run();
-  done();
+  return new Promise((resolve, reject) => {
+    ffmpeg
+      .input("./mid.gif")
+      .outputOptions([
+        "-pix_fmt yuv420p",
+        "-c:v libx264",
+        "-movflags +faststart",
+        "-filter:v crop='floor(in_w/2)*2:floor(in_h/2)*2'",
+      ])
+      .noAudio()
+      .output(`${uuidv4()}.mp4`)
+      .on("end", () => {
+        resolve();
+      })
+      .on("error", (e) => {
+        return reject(new Error(e));
+      })
+      .run();
+  });
 });
 
 async function main() {
   const numberOfRuns = [...Array(10)];
 
-  numberOfRuns.map(() => videoQueue.add({ video: "./big.gif" }));
+  numberOfRuns.map(async () => await videoQueue.add({ video: "./big.gif" }));
 }
 
 main()
